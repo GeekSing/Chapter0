36a964521e5f79d5e4bea33c0522e397cbb2eed2
jiexinLi <1904859680@qq.com>
2022-08-15 22:42:31 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 5; // 确定 variable x data tyep =>integer
+        x += 1;//语法糖
+        System.out.println(x);
+    }
+}
